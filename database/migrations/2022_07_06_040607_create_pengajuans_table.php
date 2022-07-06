e74eb93ff0f3ff6f592aa5df1b65b7f27afa3aea
auliandarih <t.posseidon@gmail.com>
2022-07-06 11:23:51 +0700

    permulaan

---
+++ database/migrations/2022_07_06_040607_create_pengajuans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengajuans', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->bigInteger('events_id');
+            $table->string('deskripsi');
+            $table->integer('harga');
+            $table->integer('qty');
+            $table->integer('jumlah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengajuans');
+    }
+};
